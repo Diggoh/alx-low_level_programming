5b4ad76bcadf424893b0c727fa8e8fc277a6dbdc
Godwill <gtdembo@gmail.com>
2022-10-05 01:06:38 -0700

    Task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -8,9 +8,9 @@
  * @height: An input integer at number of rows
  * Return: pointer to a 2D array, NULL on failure
  */
-int *alloc_grid(int width, int height)
+int **alloc_grid(int width, int height)
 {
-	int *array, i = 0, j = 0;
+	int **array, i = 0, j = 0;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
